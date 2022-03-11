6f203a08b3d259eac94f02ab15c8152a129453b9
chakravarthy <m.chakra473@gmail.com>
2022-03-11 16:11:02 +0530

    initialized

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace empwageassignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee wage calculation!");
         }
     }
 }
