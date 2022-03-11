9ff83e8debd7017a6aca60ab5aa225a8a08fee8d
chakravarthy <m.chakra473@gmail.com>
2022-03-11 16:13:55 +0530

    added uc1 to the uc1 branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace empwageassignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int isPresent = 1;
+            int isAbsent = 0;
+            Random random = new Random();
+            int randomCheck = random.Next(0, 2);
+            if (randomCheck == isAbsent)
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+
+            }
         }
     }
 }
