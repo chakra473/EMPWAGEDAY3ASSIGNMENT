2f7bce43e1a793a7392ab951165cbc86ddadab6c
chakravarthy <m.chakra473@gmail.com>
2022-03-17 14:27:54 +0530

    added empwagecalculation method to uc7

--- Program.cs
+++ Program.cs
@@ -7,6 +7,35 @@ namespace empwageassignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage calculation!");
+            EmpWageCalculation();
         }
+        public static void EmpWageCalculation()
+        {
+            int isPartTime = 1, randomCheck, isFullTime = 2, totalSalary = 0, salary, ratePerHr = 20, numOfWorkingDays = 20, day = 1,maxWorkingHrs=200, empHrs=0;
+            while (day <= numOfWorkingDays&&empHrs<=maxWorkingHrs)
+            {
+                Random random = new Random();
+                randomCheck = random.Next(0, 3);
+                switch (randomCheck)
+                {
+                    case 2:
+                        empHrs = 8;
+                        break;
+
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                salary = empHrs * ratePerHr;
+                totalSalary = totalSalary + salary;
+                Console.WriteLine($"totalsalary after day{day} is {totalSalary}");
+                day++;
+            }
+        }
+
     }
 }
