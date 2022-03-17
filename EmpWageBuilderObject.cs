855023ece049b03fc1f4b39d51619e8f10e9f670
chakravarthy <m.chakra473@gmail.com>
2022-03-17 16:15:03 +0530

    added multiple companies employee wage and save  to uc9

---
+++ EmpWageBuilderObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empwageassignment
+{
+    internal class EmpWageBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public string Company;
+        public int NumberOfWorkingDays;
+        public int MaxWorkingHrs;
+        public int RatePerHr;
+        public int TotalSalary=0;
+        public EmpWageBuilderObject(string company, int ratePerHr, int numOfWorkingDays, int maxWorkingHrs)
+        {
+            this.Company = company;
+            this.RatePerHr = ratePerHr;
+            this.NumberOfWorkingDays = numOfWorkingDays;
+            this.MaxWorkingHrs = maxWorkingHrs;
+        }
+        public  void EmpWageCalculation()
+        {
+            int  randomCheck, totalSalary = 0, salary, day = 1, empHrs = 0;
+            while (day <= this.NumberOfWorkingDays && empHrs <= this.MaxWorkingHrs)
+            {
+                Random random = new Random();
+                randomCheck = random.Next(0, 3);
+                switch (randomCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                salary = empHrs * this.RatePerHr;
+                totalSalary = totalSalary + salary;
+                Console.WriteLine($"totalsalary after day{day} is {totalSalary}");
+                day++;
+            }
+            this.TotalSalary = totalSalary;
+        }
+        public  string toString()
+        {
+            string result= "total emp wage for company " + this.Company + " is " + this.TotalSalary;
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,37 +7,14 @@ namespace empwageassignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage calculation!");
-            EmpWageCalculation("reliance",100,30,200);
-            EmpWageCalculation("dmart", 200, 25, 200);
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("dmart", 200, 25, 200);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("reliance", 250, 30, 300);
+            dmart.EmpWageCalculation();
+            Console.WriteLine(dmart.toString());
+            reliance.EmpWageCalculation();
+            Console.WriteLine(reliance.toString());
+           
         }
-        public static void EmpWageCalculation(string company,int ratePerHr,int numOfWorkingDays,int maxWorkingHrs)
-        {
-            int isPartTime = 1, randomCheck, isFullTime = 2, totalSalary = 0, salary,day = 1, empHrs=0;
-            while (day <= numOfWorkingDays&&empHrs<=maxWorkingHrs)
-            {
-                Random random = new Random();
-                randomCheck = random.Next(0, 3);
-                switch (randomCheck)
-                {
-                    case 2:
-                        empHrs = 8;
-                        break;
-
-                    case 1:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-
-                salary = empHrs * ratePerHr;
-                totalSalary = totalSalary + salary;
-                Console.WriteLine($"totalsalary after day{day} is {totalSalary}");
-                day++;
-            }
-            Console.WriteLine("total employee Wage for company "+company+" is "+totalSalary);
-        }
-
+        
     }
 }
